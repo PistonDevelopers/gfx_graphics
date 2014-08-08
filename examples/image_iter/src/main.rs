5f0e165aca43b202b85fcd689fc96cceb4edceb6
Sven Nilsen <bvssvni@gmail.com>
2014-08-08 18:20:48 +0200

    Added example
    Closes https://github.com/PistonDevelopers/gfx_graphics/issues/6

---
+++ examples/image_iter/src/main.rs
@@ -0,0 +1,54 @@
+
+#![feature(globs)]
+
+extern crate graphics;
+extern crate piston;
+extern crate sdl2_game_window;
+extern crate gfx_graphics;
+
+use gfx_graphics::{
+    Gfx2d,
+    Texture,
+};
+use sdl2_game_window::GameWindowSDL2;
+use graphics::*;
+use piston::{
+    AssetStore,
+    GameIterator,
+    GameIteratorSettings,
+    GameWindowSettings,
+    Render,
+};
+
+fn main() {
+    let mut window = GameWindowSDL2::new(
+        GameWindowSettings {
+            title: "Image".to_string(),
+            size: [300, 300],
+            fullscreen: false,
+            exit_on_esc: true,
+        }
+    );
+
+    let asset_store = AssetStore::from_folder("../bin/assets");
+
+    let image = asset_store.path("rust-logo.png").unwrap();
+    let image = Texture::from_path(&image).unwrap();
+    let game_iter_settings = GameIteratorSettings {
+            updates_per_second: 120,
+            max_frames_per_second: 60,
+        };
+    let ref mut g = Gfx2d::new();
+    for e in GameIterator::new(&mut window, &game_iter_settings) {
+        match e {
+            Render(args) => {
+                // gl.viewport(0, 0, args.width as i32, args.height as i32);
+
+                let c = Context::abs(args.width as f64, args.height as f64);
+                c.rgb(1.0, 1.0, 1.0).draw(g);
+                    c.image(&image).draw(g);
+            },
+            _ => {},
+        }
+    }
+}
--- src/texture.rs
+++ src/texture.rs
@@ -1,4 +1,5 @@
-
+use image;
+use image::GenericImage;
 use graphics::{
     ImageSize
 };
@@ -7,6 +8,29 @@ use graphics::{
 pub struct Texture {
     width: u32,
     height: u32
+}
+
+impl Texture {
+    /// Creates a texture from path.
+    pub fn from_path(path: &Path) -> Result<Texture, String> {
+        let img = match image::open(path) {
+            Ok(img) => img,
+            Err(e)  => return Err(format!("Could not load '{}': {}",
+                path.filename_str().unwrap(), e)),
+        };
+
+        match img.color() {
+            image::RGBA(8) => {},
+            c => fail!("Unsupported color type {} in png", c),
+        };
+
+        let (width, height) = img.dimensions();
+
+        Ok(Texture {
+            width: width,
+            height: height
+        })
+    }
 }
 
 impl ImageSize for Texture {
