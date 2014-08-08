bec4d8ec6d32745d5b2e440ca03e3bd77da95510
Sven Nilsen <bvssvni@gmail.com>
2014-08-08 17:50:51 +0200

    Added basic structs
    Closes https://github.com/PistonDevelopers/gfx_graphics/issues/4

---
+++ src/gfx2d.rs
@@ -0,0 +1,13 @@
+
+use graphics::{
+    BackEnd,
+};
+
+use Texture;
+
+/// The graphics back-end.
+pub struct Gfx2d;
+
+impl BackEnd<Texture> for Gfx2d {
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 #![crate_name = "gfx_graphics"]
 #![deny(missing_doc)]
 
-//! Documentation goes here.
+//! The implementation of a Rust-Graphics back-end using gfx-rs.
 
+extern crate graphics;
+extern crate gfx;
+extern crate image;
+
+pub use gfx2d::Gfx2d;
+pub use texture::Texture;
+
+mod gfx2d;
+mod texture;
