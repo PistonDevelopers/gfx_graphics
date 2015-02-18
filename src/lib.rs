8c45dde4938a446371c6bdb43cfeb56bf990eef8
Dzmitry Malyshau <kvarkus@gmail.com>
2015-02-17 23:39:01 -0500

    Implemented CharacterCache

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![feature(plugin)]
+#![feature(plugin, path, std_misc)]
 #![plugin(gfx_macros)]
 
 //! The implementation of a Rust-Graphics back-end using gfx-rs.
@@ -7,13 +7,16 @@
 #[macro_use]
 extern crate gfx_macros;
 extern crate gfx;
+extern crate gfx_texture;
 extern crate graphics;
 extern crate image;
-extern crate gfx_texture;
+extern crate freetype;
 
 pub use gfx_texture::Texture;
 
 pub use g2d::G2D;
 pub use g2d::GraphicsBackEnd;
+pub use glyph::GlyphCache;
 
 mod g2d;
+mod glyph;
