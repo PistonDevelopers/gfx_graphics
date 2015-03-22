2c808c273a70ef721caef7337771d87bd247112a
Sven Nilsen <bvssvni@gmail.com>
2015-03-22 11:25:52 +0100

    Copied "image_test.rs" to "draw_state_test.rs"

---
+++ examples/draw_state_test.rs
@@ -0,0 +1,66 @@
+#![feature(old_path)]
+
+extern crate piston;
+extern crate graphics;
+extern crate sdl2_window;
+extern crate gfx_device_gl;
+extern crate gfx_graphics;
+
+use piston::quack::Get;
+use std::cell::RefCell;
+use gfx_graphics::gfx::traits::*;
+use gfx_graphics::{
+    Gfx2d,
+    Texture,
+    gfx
+};
+use sdl2_window::{ Sdl2Window, OpenGL };
+
+fn main() {
+    let window = Sdl2Window::new(
+        OpenGL::_3_2,
+        piston::window::WindowSettings {
+            title: "gfx_graphics: draw_state_test".to_string(),
+            size: [300, 300],
+            fullscreen: false,
+            exit_on_esc: true,
+            samples: 0,
+        }
+    );
+
+    let mut device = gfx_device_gl::GlDevice::new(Sdl2Window::get_proc_address);
+    let piston::window::Size([w, h]) = window.get();
+    let frame = gfx::Frame::new(w as u16, h as u16);
+    let mut renderer = device.create_renderer();
+
+    let rust_logo = Texture::from_path(&mut device,
+        &Path::new("./assets/rust.png")).unwrap();
+    let mut g2d = Gfx2d::new(&mut device);
+    let window = RefCell::new(window);
+    for e in piston::events(&window) {
+        use piston::event::RenderEvent;
+        if let Some(_) = e.render_args() {
+            use graphics::*;
+
+            g2d.draw(&mut renderer, &frame, |c, g| {
+                let transform = c.transform.trans(100.0, 100.0);
+
+                clear([1.0; 4], g);
+                Rectangle::new([1.0, 0.0, 0.0, 1.0])
+                    .draw([0.0, 0.0, 100.0, 100.0],
+                          &c.draw_state,
+                          c.transform,
+                          g);
+                Rectangle::new([0.0, 1.0, 0.0, 0.3])
+                    .draw([50.0, 50.0, 100.0, 100.0],
+                          &c.draw_state,
+                          c.transform,
+                          g);
+                image(&rust_logo, transform, g);
+            });
+
+            device.submit(renderer.as_buffer());
+            renderer.reset();
+       }
+    }
+}
