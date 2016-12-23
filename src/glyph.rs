PistonDevelopers/gfx_graphics

commit 1305ef86d266a4913e04183faaf16024b36d384f

Fixed glyph artifacts
- Added “text_test” example

src/glyph.rs
@@ -105,20 +105,20 @@ impl<R, F> CharacterCache for GlyphCache<R, F> where
                 let bounding_box = glyph.exact_bounding_box().unwrap_or(rt::Rect{min: rt::Point{x: 0.0, y: 0.0}, max: rt::Point{x: 0.0, y: 0.0} });
                 let glyph = glyph.positioned(rt::point(0.0, 0.0));
                 let pixel_bounding_box = glyph.pixel_bounding_box().unwrap_or(rt::Rect{min: rt::Point{x: 0, y: 0}, max: rt::Point{x: 0, y: 0} });
-                let pixel_bb_width = pixel_bounding_box.width();
-                let pixel_bb_height = pixel_bounding_box.height();
+                let pixel_bb_width = pixel_bounding_box.width() + 2;
+                let pixel_bb_height = pixel_bounding_box.height() + 2;
 
                 let mut image_buffer = Vec::<u8>::new();
                 image_buffer.resize((pixel_bb_width * pixel_bb_height) as usize, 0);
                 glyph.draw(|x, y, v| {
-                   let pos = (x + y * (pixel_bb_width as u32)) as usize;
+                   let pos = ((x+1) + (y+1) * (pixel_bb_width as u32)) as usize;
                    image_buffer[pos] = (255.0 * v) as u8;
                 });
 
                 let &mut (offset, size, ref texture) = v.insert((
                     [
-                        bounding_box.min.x as Scalar,
-                        -pixel_bounding_box.min.y as Scalar,
+                        bounding_box.min.x as Scalar + 1.0,
+                        -pixel_bounding_box.min.y as Scalar + 1.0,
                     ],
                     [
                         h_metrics.advance_width as Scalar,

Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 
 name = "piston2d-gfx_graphics"
-version = "0.33.0"
+version = "0.33.1"
 authors = ["bvssvni <bvssvni@gmail.com>"]
 keywords = ["graphics", "2d", "gfx", "piston"]
 description = "A Gfx 2D back-end for the Piston game engine"

examples/text_test.rs
@@ -1,0 +1,75 @@
+extern crate piston;
+extern crate graphics;
+extern crate gfx_graphics;
+extern crate gfx;
+extern crate gfx_device_gl;
+extern crate glutin_window;
+
+use glutin_window::{GlutinWindow, OpenGL};
+use gfx::traits::*;
+use gfx::Typed;
+use gfx::format::{DepthStencil, Formatted, Srgba8};
+use std::path::Path;
+use piston::window::{OpenGLWindow, Window, WindowSettings, Size};
+use piston::input::{AfterRenderEvent, RenderEvent};
+use piston::event_loop::Events;
+use gfx_graphics::{Gfx2d, GlyphCache};
+
+fn main() {
+    let opengl = OpenGL::V3_2;
+    let size = Size { width: 500, height: 300 };
+    let samples = 4;
+    let ref mut window: GlutinWindow =
+        WindowSettings::new("gfx_graphics: text_test", size)
+        .exit_on_esc(true)
+        .opengl(opengl)
+        .samples(samples)
+        .build().unwrap();
+
+    let (mut device, mut factory) = gfx_device_gl::create(|s|
+        window.get_proc_address(s) as *const std::os::raw::c_void);
+
+    let mut glyph_cache = GlyphCache::new(
+        Path::new("assets/FiraSans-Regular.ttf"),
+        factory.clone()
+    ).unwrap();
+
+    // Create the main color/depth targets.
+    let draw_size = window.draw_size();
+    let aa = samples as gfx::tex::NumSamples;
+    let dim = (draw_size.width as u16, draw_size.height as u16, 1, aa.into());
+    let color_format = <Srgba8 as Formatted>::get_format();
+    let depth_format = <DepthStencil as Formatted>::get_format();
+    let (output_color, output_stencil) =
+        gfx_device_gl::create_main_targets_raw(dim,
+                                               color_format.0,
+                                               depth_format.0);
+    let output_color = Typed::new(output_color);
+    let output_stencil = Typed::new(output_stencil);
+
+    let mut encoder = factory.create_command_buffer().into();
+    let mut g2d = Gfx2d::new(opengl, &mut factory);
+    let mut events = window.events();
+
+    while let Some(e) = events.next(window) {
+        if let Some(args) = e.render_args() {
+            g2d.draw(&mut encoder, &output_color, &output_stencil, args.viewport(), |c, g| {
+                use graphics::*;
+
+                clear([1.0; 4], g);
+                text::Text::new_color([0.0, 0.5, 0.0, 1.0], 32).draw(
+                    "Hello gfx_graphics!",
+                    &mut glyph_cache,
+                    &DrawState::default(),
+                    c.transform.trans(10.0, 100.0),
+                    g
+                );
+            });
+            encoder.flush(&mut device);
+        }
+
+        if let Some(_) = e.after_render_args() {
+            device.cleanup();
+        }
+    }
+}

commit 4c6e7b0f572c4143b69716c8dacb23d9b5d375a2

Fix bug causing text to be drawn 2 pixels off
It appears that 1305ef86d266a4913e04183faaf16024b36d384f introduced a bug
causing text to be drawn 2 pixels to the right of where it should be.

src/glyph.rs
@@ -117,7 +117,7 @@ impl<R, F> CharacterCache for GlyphCache<R, F> where
 
                 let &mut (offset, size, ref texture) = v.insert((
                     [
-                        bounding_box.min.x as Scalar + 1.0,
+                        bounding_box.min.x as Scalar - 1.0,
                         -pixel_bounding_box.min.y as Scalar + 1.0,
                     ],
                     [